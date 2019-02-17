0e70154a068310690092c8cb8741c0c2d94224e3
Curt Polack <curtp67@gmail.com>
2019-02-17 14:58:44 +0100

    Added Support for Arduino Due
    Support for Arduino Due with Bossa

---
+++ gateway_code/open_nodes/node_arduino_due.py
@@ -0,0 +1,33 @@
+# -*- coding:utf-8 -*-
+
+# This file is a part of IoT-LAB gateway_code
+# Copyright (C) 2015 INRIA (Contact: admin@iot-lab.info)
+# Contributor(s) : see AUTHORS file
+#
+# This software is governed by the CeCILL license under French law
+# and abiding by the rules of distribution of free software.  You can  use,
+# modify and/ or redistribute the software under the terms of the CeCILL
+# license as circulated by CEA, CNRS and INRIA at the following URL
+# http://www.cecill.info.
+#
+# As a counterpart to the access to the source code and  rights to copy,
+# modify and redistribute granted by the license, users are provided only
+# with a limited warranty  and the software's author,  the holder of the
+# economic rights,  and the successive licensors  have only  limited
+# liability.
+#
+# The fact that you are presently reading this means that you have had
+# knowledge of the CeCILL license and that you accept its terms.
+
+""" Open Node Arduino Due experiment implementation """
+
+from gateway_code.config import static_path
+from gateway_code.open_nodes.common.node_bossac import NodeBossacBase
+
+
+class NodeArduinoDue(NodeBossacBase):
+    """ Open node Arduino Due implementation """
+
+    TYPE = 'arduino_due'
+    FW_IDLE = static_path('arduino-due_idle.elf')
+    FW_AUTOTEST = static_path('arduino-due_autotest.elf')
